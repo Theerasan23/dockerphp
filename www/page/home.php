0e0e33cbbccd030acc0a7558662c5e72a608a333
Theerasan23 <data.theerasan@gmail.com>
2025-04-30 16:35:39 +0700

    add tailwind

--- www/page/home.php
+++ www/page/home.php
@@ -1,1 +1,3 @@
-<?php echo "Home"; ?>
+<div class="flex items-center w-[40%] justify-center shadow-2xl">
+  <h1 class="text-2xl font-bold text-blue-600">Hello World</h1>
+</div>
---
+++ www/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.php",
+    "./src/**/*.php",
+    "./*.php",
+    "./view/**/*.php",
+    "./page/**/*.php",
+    "./controller/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
